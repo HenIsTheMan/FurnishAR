fe663df335ca1904b4a4da22735b54bb60875afb
Unknown <193541T@mymail.nyp.edu.sg>
2021-06-04 14:10:19 +0800

    ShowHidePasswordButton.cs and InputField.png

---
+++ Assets/Scripts/App/ShowHidePasswordButton.cs
@@ -0,0 +1,57 @@
+using TMPro;
+using UnityEngine;
+using static TMPro.TMP_InputField;
+
+namespace FurnishAR.App {
+    internal sealed class ShowHidePasswordButton: MonoBehaviour {
+        #region Fields
+
+        private bool isPasswordHidden;
+
+        [SerializeField]
+        private TextMeshProUGUI showHidePasswordText;
+
+        [SerializeField]
+        private TMP_InputField inputField;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        internal ShowHidePasswordButton(): base() {
+            isPasswordHidden = true;
+
+            showHidePasswordText = null;
+
+            inputField = null;
+        }
+
+        static ShowHidePasswordButton() {
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+        #endregion
+
+        public void OnClick() {
+            isPasswordHidden = !isPasswordHidden;
+
+            if(isPasswordHidden) {
+                showHidePasswordText.SetText("Show Password");
+
+                inputField.contentType = ContentType.Password;
+            } else {
+                showHidePasswordText.SetText("Hide Password");
+
+                inputField.contentType = ContentType.Standard;
+            }
+
+            inputField.textComponent.SetAllDirty();
+            //inputField.Select();
+        }
+    }
+}
