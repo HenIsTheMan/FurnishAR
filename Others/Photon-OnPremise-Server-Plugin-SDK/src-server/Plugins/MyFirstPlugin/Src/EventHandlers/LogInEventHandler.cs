01f1bd1b83842d979902d30ba52385cac4b9eedd
Unknown <193541T@mymail.nyp.edu.sg>
2021-06-07 11:39:47 +0800

    Diff session tokens (untested)

--- Others/Photon-OnPremise-Server-Plugin-SDK/src-server/Plugins/MyFirstPlugin/Src/EventHandlers/LogInEventHandler.cs
+++ Others/Photon-OnPremise-Server-Plugin-SDK/src-server/Plugins/MyFirstPlugin/Src/EventHandlers/LogInEventHandler.cs
@@ -122,7 +122,7 @@ namespace MyFirstPlugin {
 							string sessionToken = string.Empty;
 
 							for(int j = 0; j < sessionTokenLen; ++j) {
-								sessionToken += (char)PseudorandRange(33.0f, 127.0f, (uint)j ^ 2645u);
+								sessionToken += (char)PseudorandRange(33.0f, 127.0f, (uint)(user.ID ^ j ^ 2645));
 							}
 
 							logInData.sessionToken = sessionToken;
