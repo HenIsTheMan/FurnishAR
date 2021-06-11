851b452347d822f360f772428e919b16cb5a3248
Unknown <193541T@mymail.nyp.edu.sg>
2021-06-11 18:08:09 +0800

    CancelButton.cs

---
+++ Assets/Scripts/App/CancelButton.cs
@@ -0,0 +1,62 @@
+using FurnishAR.Anim;
+using UnityEngine;
+
+namespace FurnishAR.App {
+    internal sealed class CancelButton: MonoBehaviour {
+        #region Fields
+
+        [SerializeField]
+        private PlacementMarkerControl placementMarkerControl;
+
+        [SerializeField]
+        private RectTransformTranslateAnim translateAnim;
+
+        [SerializeField]
+        private GameObject acctButtonGO;
+
+        [SerializeField]
+        private GameObject thinUpArrowGO;
+
+        [SerializeField]
+        private GameObject swipeDetectorGO;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        internal CancelButton(): base() {
+            placementMarkerControl = null;
+
+            translateAnim = null;
+
+            acctButtonGO = null;
+            thinUpArrowGO = null;
+
+            swipeDetectorGO = null;
+        }
+
+        static CancelButton() {
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+        #endregion
+
+        public void OnClick() {
+            placementMarkerControl.PlacementMarkerParentTransform.gameObject.SetActive(false);
+
+            translateAnim.IsUpdating = true;
+
+            acctButtonGO.SetActive(true);
+            thinUpArrowGO.SetActive(true);
+
+            placementMarkerControl.shldRaycast = false;
+
+            swipeDetectorGO.SetActive(true);
+        }
+    }
+}
