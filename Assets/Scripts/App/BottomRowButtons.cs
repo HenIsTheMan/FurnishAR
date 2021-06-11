8cbfb9dc75298bb5c37743764175854baa33aca5
Unknown <193541T@mymail.nyp.edu.sg>
2021-06-11 15:46:30 +0800

    BIG but hmmmm

---
+++ Assets/Scripts/App/BottomRowButtons.cs
@@ -0,0 +1,46 @@
+using FurnishAR.Generic;
+using UnityEngine;
+using static FurnishAR.Generic.InitIDs;
+
+namespace FurnishAR.App {
+    internal sealed class BottomRowButtons: MonoBehaviour {
+        #region Fields
+
+        [SerializeField]
+        private InitControl initControl;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        internal BottomRowButtons(): base() {
+            initControl = null;
+        }
+
+        static BottomRowButtons() {
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void OnEnable() {
+            initControl.AddMethod((uint)InitID.BottomRowButtons, Init);
+        }
+
+        private void OnDisable() {
+            initControl.RemoveMethod((uint)InitID.BottomRowButtons, Init);
+        }
+
+        #endregion
+
+        private void Init() {
+            Generic.Console.Log("here");
+
+            ((RectTransform)transform).localPosition = new Vector3(0.0f, -1907.0f, 0.0f); //Workaround
+        }
+    }
+}
--- Assets/Scripts/Generic/Unattachable/InitIDs.cs
+++ Assets/Scripts/Generic/Unattachable/InitIDs.cs
@@ -8,6 +8,7 @@ namespace FurnishAR.Generic {
 
 			Instancer,
 			AbstractAnim,
+			BottomRowButtons,
 			TextCurve,
 			TextPtrOverLineOfWords,
 			CamOrbital,
