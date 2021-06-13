4a861375e633cf1126f4b357ada6ba40c3f931ce
Unknown <193541T@mymail.nyp.edu.sg>
2021-06-13 20:52:27 +0800

    UndoButton.cs (create) and RedoButton.cs (create)

---
+++ Assets/Scripts/App/RedoButton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace FurnishAR.App {
+    internal sealed class RedoButton: MonoBehaviour {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        internal RedoButton(): base() {
+        }
+
+        static RedoButton() {
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+        #endregion
+
+        public void OnClick() {
+        }
+    }
+}
---
+++ Assets/Scripts/App/UndoButton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace FurnishAR.App {
+    internal sealed class UndoButton: MonoBehaviour {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        internal UndoButton(): base() {
+        }
+
+        static UndoButton() {
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+        #endregion
+
+        public void OnClick() {
+        }
+    }
+}
